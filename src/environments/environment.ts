6ecb59c692255e87df3fd45b52c25d42f923d629
Dharshan <66472781+im-kronos@users.noreply.github.com>
2024-03-10 22:03:15 +0530

    hosted project

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+
+
+import { EnvironmentModel } from './environment-model';
+
+export const environment: EnvironmentModel = {
+  production: false,
+  apiUrl: '/assets/data' // 'http://localhost:3000'
+};
+
+
